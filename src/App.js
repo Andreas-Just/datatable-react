b39f0dfa103985980a363408ae4c21cd07bdf1db
Andreas_Just <andreas.wq@gmail.com>
2019-03-24 12:36:22 +0200

    People was added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Datatable from './Datatable';
+
+const peopleColumnConfig = {
+  name: { // Только для тех ключей которые есть в columnConfig будут колонки в таблице
+    title: 'Имя', // в таблице колонка будет так называться
+    isSortable: true, // по этой колонке можно сортировать
+    isSearchable: true, // поиск будет проверять эту и последнюю колонки
+  },
+  sex: {
+    title: 'Пол',
+  },
+  born: {
+    title: 'Год рождения',
+    isSortable: true,
+  },
+  died: {
+    title: 'Год смерти',
+    isSortable: true,
+  },
+  father: {
+    title: 'Отец',
+    isSortable: true,
+    isSearchable: true,
+  },
+  mother: {
+    title: 'Мать',
+    isSortable: true,
+    isSearchable: true,
+  }
+};
 
 class App extends Component {
-  render() {
+  state = {
+    people: [],
+    config: peopleColumnConfig,
+  };
+
+  async componentDidMount() {
+    const response = await fetch('https://andreas-just.github.io/library-json/people/people.json');
+    const people = await response.json();
+
+    this.setState({ people });
+  }
+
+  render () {
+    const { people, config } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Datatable</h1>
+        <Datatable
+          items={people}
+          config={config}
+        />
       </div>
     );
   }
---
+++ src/Datatable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Datatable = ({ config, items }) => (
+  <div className="Datatable">
+    <table>
+      <thead>
+      <tr>
+        { Object.values(config).map((value) => (
+          <th className={value.isSortable ? 'sortable-column' : ''}>
+            {value.title}
+          </th>
+        ))}
+      </tr>
+      </thead>
+      <tbody>
+      {items.map(item =>
+        <Row item={item} config={config}/>
+      )}
+      </tbody>
+    </table>
+  </div>
+);
+
+const Row = ({ item, config }) => (
+  <tr>
+    { Object.keys(config).map((key) => (
+      <td>{item[key]}</td>
+    ))}
+  </tr>
+);
+
+const Cell = ({children}) => (
+  <td>{children}</td>
+);
+
+export default Datatable;
