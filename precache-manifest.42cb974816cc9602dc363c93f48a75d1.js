5e37647d03f616cf2a9cba3b76795e7e071e5227
Andreas_Just <andreas.wq@gmail.com>
2019-06-13 10:19:47 +0300

    Updates

---
+++ precache-manifest.42cb974816cc9602dc363c93f48a75d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7384926bd2f11e1f67c20c98fa8ac9",
+    "url": "/datatable-react/index.html"
+  },
+  {
+    "revision": "6e734469a95722aa0c5e",
+    "url": "/datatable-react/static/css/main.47e35c90.chunk.css"
+  },
+  {
+    "revision": "62ba9f3a466786d3622e",
+    "url": "/datatable-react/static/js/2.4f64b287.chunk.js"
+  },
+  {
+    "revision": "6e734469a95722aa0c5e",
+    "url": "/datatable-react/static/js/main.de49a6e0.chunk.js"
+  },
+  {
+    "revision": "13fc41ea1f2e115b3a99",
+    "url": "/datatable-react/static/js/runtime~main.cf23a1ca.js"
+  }
+]);
